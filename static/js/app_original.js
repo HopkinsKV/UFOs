d1f063af41862bec495d68940e2dc263cd92b337
Katie Hopkins <HopkinsKV@gmail.com>
2022-01-10 16:17:10 -0600

    updt index css imgs app

---
+++ static/js/app_original.js
@@ -0,0 +1,79 @@
+// This code builds the HTML table and fills it with data from data.js.
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Function to build table and clear data, 
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        });
+    });
+  }
+
+  // Button clicking function
+function handleClick() {
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+   // If a date was entered, filter the data using that date.
+  if (date) {
+    // Apply `filter` & return rows where `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  };
+
+   // Rebuild the table using the filtered data
+  buildTable(filteredData);
+}
+// Listen for click
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build table when page loads
+buildTable(tableData);
+
+
+
+function updateFilters() {
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+   // If a date was entered, filter the data using that date.
+  if (date) {
+    // Apply `filter` & return rows where `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  };
+
+   // Rebuild the table using the filtered data
+  buildTable(filteredData);
+}
+
+
+if (y < 5) {
+  console.log("x is less than 10 and 
+  y is less than 5");
+  }
+  else if (y === 5) {
+  console.log("x is less than 10 and 
+  y is equal to 5");
+  }
+  else {
+  console.log("x is less than 10 and 
+  y is greater than 5");
+  }
+  
