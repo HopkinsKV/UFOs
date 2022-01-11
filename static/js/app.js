6907fb7ac195797cbc7f7d94cb01fe8bc05b68fd
Katie Hopkins <HopkinsKV@gmail.com>
2022-01-11 12:03:19 -0600

    refactoring pt 9

--- static/js/app.js
+++ static/js/app.js
@@ -56,32 +56,14 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    let date = d3.select("#datetime").property("value");
-    let city = d3.select("#city").property("value");
-    let state = d3.select("#state").property("value");
-    let country = d3.select("#country").property("value");
-    let shape = d3.select("#shape").property("value");
+
     let filteredData = tableData;
       
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    if (date || city || state || country || shape) {
-      if (date) {
-          filteredData = filteredData.filter(row => row.datetime === date);
-      }
-      if (city) {
-          filteredData = filteredData.filter(row => row.city === city);
-      }
-      if (state) {
-          filteredData = filteredData.filter(row => row.state === state);
-      }
-      if (country) {
-          filteredData = filteredData.filter(row => row.country === country);
-      }
-      if (shape) {
-          filteredData = filteredData.filter(row => row.shape === shape);
-      }
-  }
+    Object.entries(changedFilters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
 
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);  
